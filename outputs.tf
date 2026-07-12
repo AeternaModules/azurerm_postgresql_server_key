53c014ebfe7fb230172b625ed28da23049728f5a
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:44 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,9 @@
-output "postgresql_server_keys" {
-  description = "All postgresql_server_key resources"
-  value       = azurerm_postgresql_server_key.postgresql_server_keys
-}
 output "postgresql_server_keys_key_vault_key_id" {
-  description = "List of key_vault_key_id values across all postgresql_server_keys"
-  value       = [for k, v in azurerm_postgresql_server_key.postgresql_server_keys : v.key_vault_key_id]
+  description = "Map of key_vault_key_id values across all postgresql_server_keys, keyed the same as var.postgresql_server_keys"
+  value       = { for k, v in azurerm_postgresql_server_key.postgresql_server_keys : k => v.key_vault_key_id }
 }
 output "postgresql_server_keys_server_id" {
-  description = "List of server_id values across all postgresql_server_keys"
-  value       = [for k, v in azurerm_postgresql_server_key.postgresql_server_keys : v.server_id]
+  description = "Map of server_id values across all postgresql_server_keys, keyed the same as var.postgresql_server_keys"
+  value       = { for k, v in azurerm_postgresql_server_key.postgresql_server_keys : k => v.server_id }
 }
 
