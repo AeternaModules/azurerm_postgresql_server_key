fbaa890e95560d4fde876b3d5b55fedeec222e42
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:50:09 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
 output "postgresql_server_keys_id" {
   description = "Map of id values across all postgresql_server_keys, keyed the same as var.postgresql_server_keys"
-  value       = { for k, v in azurerm_postgresql_server_key.postgresql_server_keys : k => v.id }
+  value       = { for k, v in azurerm_postgresql_server_key.postgresql_server_keys : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "postgresql_server_keys_key_vault_key_id" {
   description = "Map of key_vault_key_id values across all postgresql_server_keys, keyed the same as var.postgresql_server_keys"
-  value       = { for k, v in azurerm_postgresql_server_key.postgresql_server_keys : k => v.key_vault_key_id }
+  value       = { for k, v in azurerm_postgresql_server_key.postgresql_server_keys : k => v.key_vault_key_id if v.key_vault_key_id != null && length(v.key_vault_key_id) > 0 }
 }
 output "postgresql_server_keys_server_id" {
   description = "Map of server_id values across all postgresql_server_keys, keyed the same as var.postgresql_server_keys"
-  value       = { for k, v in azurerm_postgresql_server_key.postgresql_server_keys : k => v.server_id }
+  value       = { for k, v in azurerm_postgresql_server_key.postgresql_server_keys : k => v.server_id if v.server_id != null && length(v.server_id) > 0 }
 }
 
